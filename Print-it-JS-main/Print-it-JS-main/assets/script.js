8fbfb38f8c68959111680aea38d6fb7a1b39ccb4
Sonia Sabbahi <soniadevpro@gmail.com>
2023-10-03 21:16:53 +0200

    maj dot

--- Print-it-JS-main/Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/Print-it-JS-main/assets/script.js
@@ -1,4 +1,6 @@
-const slides = [
+
+
+	const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -16,3 +18,65 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+// Mes variables
+let nbSlides = slides.length -1;
+let departDot = 0;
+
+const arrowLeft = document.getElementById("arrowLeft") //variables flèches
+const arrowRight = document.getElementById("arrowRight")
+const bannerImg = document.getElementById("banner-img")
+const dots = document.querySelector(".dots")
+
+
+// affichage BULLET POINTS
+for (let pas = 0; pas <= nbSlides; pas++) {
+    dots.innerHTML +=
+      '<span id="dot' +
+      pas +
+      '" class="dot"  ' +
+      (pas + 1) +
+      '"></span>';
+  }
+
+  const dotList = document.querySelectorAll(".dot");
+
+
+const dotSelected = () => {
+	for (let i = 0; i <= nbSlides; i++) {
+		if (i === departDot) {
+			dotList[i].classList.remove("dot_selected");
+		}
+		else {
+			dotList[i].classList.add("dot_selected"); }
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+// Event flèches
+arrowLeft.addEventListener("click", function (){
+	
+	console.log("Flèche gauche")
+})
+
+arrowRight.addEventListener("click", function (){
+	
+	console.log("Flèche droite")
+})
+
+// bullet points
+
+
+
+
